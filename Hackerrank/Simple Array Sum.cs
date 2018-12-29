6317042b8fd599bd5286f47ea4e19c8e70ba823f
Sthuarty <sthuarty@gmail.com>
2018-12-29 19:15:58 -0200

    Update Simple Array Sum.cs

--- Hackerrank/Simple Array Sum.cs
+++ Hackerrank/Simple Array Sum.cs
@@ -1,4 +1,6 @@
 /*
+https://www.hackerrank.com/challenges/simple-array-sum/problem
+
 Given an array of integers, find the sum of its elements.
 For example, if the array ar = [1,2,3], 1 + 2 + 3 = 6, so return 6.
 
